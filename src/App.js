3bbf68eeea230b2c294c005b5ac481ef7560ca27
nikita.shaposhnikov <topru123@gmail.com>
2021-06-20 07:23:47 +0300

    Dictionary control visuals created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+class App extends React.Component {
+  state = {
+    cards: [
+      { id: 1, name: "foo" },
+      { id: 2, name: "bar" },
+      { id: 3, name: "baz" }
+    ],
+    selected: {}
+  };
+
+  handleSelect = id =>
+    this.setState(state => ({
+      selected: { ...state.selected, [id]: !state.selected[id] }
+    }));
+
+  render() {
+    const { cards, selected } = this.state;
+    return (
+      <div className="App">
+        <div id="Combiner">
+          <div id="Dictionary">
+            <div id="DictionaryContainer">
+              <textarea id="DictionaryInput" className="SolidBorderInside"/>
+            </div>
+            <div id="DictionaryControll">
+              <div id="DictionaryControllContainer">
+                <button id="DictionaryValidate" className="ControllButton SolidBorder">
+                  Validate
+                </button>
+              </div>
+            </div>
+          </div>
+          <div id="Insert">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+// <div>
+//   {cards.map(card => (
+//     <Card
+//       key={card.id}
+//       card={card}
+//       handleSelect={this.handleSelect}
+//       selected={selected[card.id]}
+//     />
+//   ))}
+// </div>
+
+const Card = ({ card, handleSelect, selected }) => {
+  const handleClick = () => handleSelect(card.id);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={selected ? "selected" : ""} onClick={handleClick}>
+      {card.id}-{card.name}
     </div>
   );
-}
+};
 
 export default App;
